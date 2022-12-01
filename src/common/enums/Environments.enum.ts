a77309c73fe21301dadfbeab2b71b32e107fe2f9
Gleb Makhankov <hlebreadm@gmail.com>
2022-12-01 21:31:23 +0300

    feat: add environments enum

---
+++ src/common/enums/Environments.enum.ts
@@ -0,0 +1,11 @@
+/**
+ * Enum for available app environments.
+ *
+ * @enum {string}
+ */
+export const enum Environments {
+  DEVELOPMENT = 'development',
+  PRODUCTION = 'production',
+  STAGING = 'staging',
+  TEST = 'test',
+}
