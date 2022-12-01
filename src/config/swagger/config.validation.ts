97f66de38ee32990b4356522c06133c6e61e7c1b
Gleb Makhankov <hlebreadm@gmail.com>
2022-12-01 22:13:23 +0300

    refactor: update swagger description

--- src/config/swagger/config.validation.ts
+++ src/config/swagger/config.validation.ts
@@ -8,7 +8,7 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   SWAGGER_URL: Joi.string().default('/api'),
   SWAGGER_TITLE: Joi.string().default('Api Docs'),
-  SWAGGER_DESCRIPTION: Joi.string().default('REST API documentation'),
+  SWAGGER_DESCRIPTION: Joi.string().default('Task Manager API documentation'),
   SWAGGER_VERSION: Joi.string().default('0.0.1'),
   SWAGGER_USER: Joi.string().default('developer'),
   SWAGGER_PASSWORD: Joi.string().default('123'),
